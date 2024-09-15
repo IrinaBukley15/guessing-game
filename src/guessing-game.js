6a4936b200faa5b0347df4260e7994c8922099ba
Irina <irenserge@mail.ru>
2024-09-15 12:14:59 +0300

    feat: add decision

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+        this.min = min;
+        this.max = max;
+        }
+    
+        guess() {
+        this.num = Math.round((this.max + this.min) /2)
+        return this.num;
+        }
+    
+        lower() {
+        this.max = this.num
+        }
+    
+        greater() {
+        this.min = this.num
+        }
 }
 
 module.exports = GuessingGame;
